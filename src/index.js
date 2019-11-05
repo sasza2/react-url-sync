72cf6a64bc8842e1db8f9472559b585dd22f17a3
sasza <sasza@onet.eu>
2019-11-05 21:44:15 +0100

    UrlSyncProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-const useUrl = () => {
+import React, { createContext} from 'react'
+import PropTypes from 'prop-types'
 
+const UrlSyncContext = createContext({})
+
+const UrlSyncProvider = (props) => {
+  const types = {
+    ...props
+  }
+  delete types.children
+
+  return (
+    <UrlSyncContext.Provider value={{ types }}>
+      {props.children}
+    </UrlSyncContext.Provider>
+  )
 }
 
-export default useUrl
+UrlSyncProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default UrlSyncProvider
