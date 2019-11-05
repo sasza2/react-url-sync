cfe0e9416f2b4fba4264be211e61341f98ec10e9
sasza <sasza@onet.eu>
2019-11-05 22:32:47 +0100

    useUrlSync

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
-import React, { createContext} from 'react'
+import React, { createContext, useCallback, useContext, useMemo} from 'react'
 import PropTypes from 'prop-types'
+import { useHistory, useLocation } from 'react-router'
+import queryString from 'query-string'
+import Factory from 'type-casting'
 
 const UrlSyncContext = createContext({})
+
+export const useUrlSync = () => {
+  const { types } = useContext(UrlSyncContext)
+
+  const history = useHistory()
+  const location = useLocation()  
+
+  const values = useMemo(() => Factory(types).cast(queryString.parse(location.search)), [types, location.search])
+
+  const urlNext = useCallback((valuesNext) => {
+    return `${location.pathname}?${queryString.stringify(Factory(types).cast(valuesNext))}${location.hash}`
+  }, [types, location])
+
+  const change = (key, value) => {
+    const valuesNext = {...values}
+    valuesNext[key] = value
+    history.replace(urlNext(valuesNext))
+  }
+
+  const changeAll = (valuesNext) => {
+    history.replace(urlNext(valuesNext))
+  }
+
+  return {
+    change,
+    changeAll,
+    values,
+  }
+}
 
 const UrlSyncProvider = (props) => {
   const types = {
